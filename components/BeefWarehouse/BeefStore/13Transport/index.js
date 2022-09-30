457e34e463b5e388cc0c5831f3d5b67938acae3c
bykim17 <piyawat800@gmail.com>
2022-09-30 23:16:35 +0700

    no message

--- components/BeefWarehouse/BeefStore/13Transport/index.js
+++ components/BeefWarehouse/BeefStore/13Transport/index.js
@@ -224,10 +224,10 @@ const Index = () => {
                             padding: "5px 30px",
                         }}
                     >
-                        การแจ้งเตือน
+                        บันทึกการขนส่ง
                     </HeaderColor>
                 </div>
-                <DivBase>x
+                <DivBase>
                     <DivFrom style={{ marginBottom: "15px" }}>
                         <DivFromTop>
                             <div style={{ margin: "-3px 5px 0px 0px" }}>
