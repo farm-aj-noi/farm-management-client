353c4303148d4732428a2a3f9401ee7633bcffa4
DESKTOP-656D2VD\Keror <keroroflook@gmail.com>
2022-10-02 18:10:27 +0700

    no message

--- pages/beefgrading/history.js
+++ pages/beefgrading/history.js
@@ -1,13 +1,7 @@
 import React from "react";
 import Nav from "../../components/Beefgrading/Nav/Nav";
-<<<<<<< HEAD
 import Index from "../../components/beefgrading/history/index";
 const index = () => {
-=======
-import History from "../../components/Beefgrading/history/History";
-
-const history = () => {
->>>>>>> 457e34e463b5e388cc0c5831f3d5b67938acae3c
   return (
     <div>
       <Nav/>
